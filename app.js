916333f6f69b419e1e56c5d7351f67b3a200c5dd
Brent Klostermann <bklostermann@konradgroup.com>
2018-08-10 12:56:23 -0400

    Completed module 5

--- app.js
+++ app.js
@@ -13,9 +13,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/js')));
+app.set('views', path.join(__dirname, 'src/views'));
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', {
+    title: 'My Library',
+    list: ['a', 'b'],
+  });
 });
 
 app.listen(port, () => {
